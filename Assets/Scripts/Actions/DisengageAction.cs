f8509632b1e0b00596b60cce7811ac0298616f40
DSlayer <93089045+sharon123454@users.noreply.github.com>
2022-12-29 12:58:46 +0200

    Created scripts for actions i need to add

---
+++ Assets/Scripts/Actions/DisengageAction.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+using System;
+
+public class DisengageAction : BaseAction
+{
+    public override EnemyAIAction GetEnemyAIAction(GridPosition gridPosition)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override List<GridPosition> GetValidActionGridPositionList()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void TakeAction(GridPosition gridPosition, Action actionComplete)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override string GetActionName() { return "Disengage"; }
+
+}
