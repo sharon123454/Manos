957dfc3c6a1d55cc614d00dfd0001a4412c53450
DSlayer <93089045+sharon123454@users.noreply.github.com>
2023-01-08 01:08:49 +0200

    Added Visual locations

--- Assets/Scripts/AOEProjectile.cs
+++ Assets/Scripts/AOEProjectile.cs
@@ -5,29 +5,52 @@ using System;
 
 public class AOEProjectile : MonoBehaviour
 {
+    public static event EventHandler OnAnyAOEHit;
+
     [SerializeField] private float damage = 30;
     [SerializeField] private LayerMask damageLayer;
     [SerializeField] private float moveSpeed = 15f;
     [Tooltip("Unity world units, grid scale needs to be multiplied")]
     [SerializeField] private float damageRadius = 2f;
     [SerializeField] private float reachedTargetDistance = 0.2f;
+    [SerializeField] private float hightFromGround = 1f;
+    [SerializeField] private Transform aOEHitVFXPrefab;
+    [SerializeField] private TrailRenderer trailRenderer;
+    [SerializeField] private AnimationCurve arcYAnimationCurve;
 
     private Vector3 targetPosition;
     private Action onAOEBehaviourComplete;
+    private Vector3 positionXZ;
+    private float totalDistance;
 
     private void Update()
     {
-        Vector3 moveDir = (targetPosition - transform.position).normalized;
+        Vector3 moveDir = (targetPosition - positionXZ).normalized;
 
-        transform.position += moveDir * moveSpeed * Time.deltaTime;
+        positionXZ += moveDir * moveSpeed * Time.deltaTime;
 
-        if (Vector3.Distance(transform.position, targetPosition) < reachedTargetDistance)
+        // distance to target
+        float distance = Vector3.Distance(positionXZ, targetPosition);
+        // 1-distance as distance decreases, progress distances increases
+        float distanceNormalized = 1 - distance / totalDistance;
+
+        float maxHeight = totalDistance / 4;
+        float positionY = arcYAnimationCurve.Evaluate(distanceNormalized) * maxHeight;
+        transform.position = new Vector3(positionXZ.x, positionY, positionXZ.z);
+
+        if (Vector3.Distance(positionXZ, targetPosition) < reachedTargetDistance)
         {
             Collider[] colliderArray = Physics.OverlapSphere(targetPosition, damageRadius, damageLayer);
 
             foreach (Collider collider in colliderArray)
                 if (collider.TryGetComponent<Unit>(out Unit targetUnit))
                     targetUnit.Damage(damage);
+
+            OnAnyAOEHit?.Invoke(this, EventArgs.Empty);
+
+            trailRenderer.transform.parent = null;
+
+            Instantiate(aOEHitVFXPrefab, targetPosition + Vector3.up * hightFromGround, Quaternion.identity);
 
             Destroy(gameObject);
 
@@ -39,6 +62,10 @@ public class AOEProjectile : MonoBehaviour
     {
         this.onAOEBehaviourComplete = onAOEBehaviourComplete;
         targetPosition = LevelGrid.Instance.GetWorldPosition(targetGridPosition);
+
+        positionXZ = transform.position;
+        positionXZ.y = 0;
+        totalDistance = Vector3.Distance(positionXZ, targetPosition);
     }
 
 }
--- Assets/Scripts/ScreenShakeActions.cs
+++ Assets/Scripts/ScreenShakeActions.cs
@@ -1,19 +1,27 @@
 using System.Collections.Generic;
 using System.Collections;
 using UnityEngine;
+using System;
 
 public class ScreenShakeActions : MonoBehaviour
 {
     [SerializeField] float shootShakeIntensity = 5f;
+    [SerializeField] float aOEShakeIntensity = 8f;
 
     private void Start()
     {
         ShootAction.OnAnyShoot += ShootAction_OnAnyShoot;
+        AOEProjectile.OnAnyAOEHit += AOEProjectile_OnAnyAOEHit;
     }
 
     private void ShootAction_OnAnyShoot(object sender, ShootAction.OnSHootEventArgs e)
     {
         ScreenShake.Instance.Shake(shootShakeIntensity);
+    }
+
+    private void AOEProjectile_OnAnyAOEHit(object sender, EventArgs e)
+    {
+        ScreenShake.Instance.Shake(aOEShakeIntensity);
     }
 
 }
