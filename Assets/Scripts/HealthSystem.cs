fe54e2080597eda0c1552919a86fa74a6f609595
Eli Zaidman-PC <Zaidmaneli@gmail.com>
2023-01-15 18:17:38 +0200

    New Stats V1

--- Assets/Scripts/HealthSystem.cs
+++ Assets/Scripts/HealthSystem.cs
@@ -2,36 +2,61 @@ using System.Collections.Generic;
 using System.Collections;
 using UnityEngine;
 using System;
+using Adobe.Substance;
+using UnityEditor.PackageManager;
+using Random = System.Random;
 
 public class HealthSystem : MonoBehaviour
 {
     public event EventHandler OnDeath;
     public event EventHandler OnDamaged;
 
+
     [SerializeField] private float maxHealth = 100;
+    [SerializeField] private float maxPosture = 100;
+    [SerializeField] private float Armor = 100;
+    [SerializeField] private float evasion = 100;
     private float health;
+    private float postureAmount;
 
     private void Awake()
     {
         health = maxHealth;
     }
 
-    public float GetHealthNormalized() {return health / maxHealth;}
+    public float GetHealthNormalized() { return health / maxHealth; }
 
-    public void TakeDamage(float damage)
+    public void TakeDamage(float damage, float hitChance)
     {
-        health -= damage;
+        int DiceRoll = UnityEngine.Random.Range(0, 101);
+        if (postureAmount <= 0)
+        {
+            damage = health + Armor;
+        }
+        else
+        {
+            if ((hitChance - evasion) >= DiceRoll)
+            {
+                if (health < 0) health = 0;
 
-        if (health < 0) health = 0;
+                OnDamaged?.Invoke(this, EventArgs.Empty);
 
-        OnDamaged?.Invoke(this, EventArgs.Empty);
+                if (health == 0) Die();
+            }
+            else
+            {
+                return;
+            }
+        }
+        {
+            health -= damage;
 
-        if (health == 0) Die();
+
+        }
+
     }
-
     private void Die()
     {
         OnDeath?.Invoke(this, EventArgs.Empty);
     }
-
 }
