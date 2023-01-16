5e47f4bba368a7078faea4b3fd38d0f1f4851527
DSlayer <93089045+sharon123454@users.noreply.github.com>
2023-01-17 01:25:17 +0200

    .

--- Assets/Scripts/Actions/BaseAbility.cs
+++ Assets/Scripts/Actions/BaseAbility.cs
@@ -1,15 +1,28 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
+using System;
 
+public enum AbilityRange 
+{
+    Melee/* 0 - 1 */,
+    Close/* 0 - 4, 5-9 */,
+    Medium/* 2-4, 5 - 9, 10-15 */,
+    Long/* 5 - 15 */,
+    EffectiveAtAll/* 0 - 15 */,
+    InaccurateAtAll/* 0-15 */ 
+}
 public class BaseAbility : BaseAction
 {
-    //damage, hit chance etc
+    [SerializeField] protected AbilityRange range;
+    [SerializeField] protected bool isSpell = true;
+    [SerializeField] protected float damage, postureDamage;
+    [Range(0,100)]
+    [SerializeField] protected int hitChance, critChance, statusEffectChance;
+    //status effect? what is it?
 
-    public override string GetActionName()
+    public override void TakeAction(GridPosition gridPosition, Action actionComplete)
     {
-        throw new NotImplementedException();
     }
 
     public override EnemyAIAction GetEnemyAIAction(GridPosition gridPosition)
@@ -22,8 +35,6 @@ public class BaseAbility : BaseAction
         throw new NotImplementedException();
     }
 
-    public override void TakeAction(GridPosition gridPosition, Action actionComplete)
-    {
-        throw new NotImplementedException();
-    }
+    public override string GetActionName() { return "Ability"; }
+
 }
