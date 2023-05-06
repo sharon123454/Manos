d8171d1d9c23f36b197e8409aa68d390db17bf25
EliZaidman <zaidmaneli@gmail.com>
2023-05-06 19:14:07 +0300

    Revert For Now

--- Assets/Scripts/MouseWorld.cs
+++ Assets/Scripts/MouseWorld.cs
@@ -1,11 +1,12 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class MouseWorld : MonoBehaviour
 {
     private static MouseWorld instance;
     [SerializeField] private LayerMask mousePlaneLayerMask;
-    [SerializeField] private LayerMask Unit;
-
+    [SerializeField] private List<LayerMask> Unit;
     private void Awake()
     {
         if (instance != null && instance != this)
@@ -17,8 +18,18 @@ public class MouseWorld : MonoBehaviour
     public static Vector3 GetPosition()
     {
         Ray _ray = Camera.main.ScreenPointToRay(ManosInputController.Instance.GetPointerPosition());
-        Physics.Raycast(_ray, out RaycastHit _rayCastHit, float.MaxValue, instance.Unit);
+
+        Physics.Raycast(_ray, out RaycastHit _rayCastHit, float.MaxValue, instance.mousePlaneLayerMask);
         return _rayCastHit.point;
+        //if (_rayCastHit.transform.gameObject.layer == instance.Unit[1])
+        //{
+        //    return _rayCastHit.point;
+        //}
+        //else
+        //{
+        //    Physics.Raycast(_ray, out RaycastHit rayCastHit, float.MaxValue, instance.Unit[1]);
+        //    return rayCastHit.point;
+        //}
     }
 
 }
