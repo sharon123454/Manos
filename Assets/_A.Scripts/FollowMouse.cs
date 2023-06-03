4b010a69e34aaabdf134b01c65b4367397c594eb
DSlayer <93089045+sharon123454@users.noreply.github.com>
2023-06-03 15:17:55 +0300

    Update FollowMouse.cs

--- Assets/_A.Scripts/FollowMouse.cs
+++ Assets/_A.Scripts/FollowMouse.cs
@@ -46,41 +46,43 @@ public class FollowMouse : MonoBehaviour
 
     public void DrawLineOnPath(List<GridPosition> _pathGridPositionList)
     {
+        if (_pathGridPositionList != null && _pathGridPositionList.Count <= 1)
+            return;
+
         for (int i = 0; i < _diagLinePooler.Count; i++)
         {
             _diagLinePooler[i].transform.SetLocalPositionAndRotation(Vector3.zero, Quaternion.identity);
             _linePooler[i].transform.SetLocalPositionAndRotation(Vector3.zero, Quaternion.identity);
         }
 
-        if (_pathGridPositionList.Count > 1)
-            for (int i = 0; i < _pathGridPositionList.Count - 1; i++)
+        for (int i = 0; i < _pathGridPositionList.Count - 1; i++)
+        {
+            if (_pathGridPositionList[i] != null && _pathGridPositionList[i + 1] != null)
             {
-                if (_pathGridPositionList[i] != null && _pathGridPositionList[i + 1] != null)
+                GridPosition currentGrid = new GridPosition(_pathGridPositionList[i]._x, _pathGridPositionList[i]._z);
+                GridPosition nextGrid = new GridPosition(_pathGridPositionList[i + 1]._x, _pathGridPositionList[i + 1]._z);
+                Vector3 currentWorldPos = LevelGrid.Instance.GetWorldPosition(currentGrid);
+                Vector3 nextWorldPos = LevelGrid.Instance.GetWorldPosition(nextGrid);
+                Vector3 dir = nextWorldPos - currentWorldPos;
+
+                float rotateAngle = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg;
+
+                Vector3 fixedPosition = new Vector3(currentWorldPos.x, 2.25f, currentWorldPos.z);
+
+                if (Vector3.Distance(currentWorldPos, nextWorldPos) > 2f)
                 {
-                    GridPosition currentGrid = new GridPosition(_pathGridPositionList[i]._x, _pathGridPositionList[i]._z);
-                    GridPosition nextGrid = new GridPosition(_pathGridPositionList[i + 1]._x, _pathGridPositionList[i + 1]._z);
-                    Vector3 currentWorldPos = LevelGrid.Instance.GetWorldPosition(currentGrid);
-                    Vector3 nextWorldPos = LevelGrid.Instance.GetWorldPosition(nextGrid);
-                    Vector3 dir = nextWorldPos - currentWorldPos;
-
-                    float rotateAngle = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg;
-
-                    Vector3 fixedPosition = new Vector3(currentWorldPos.x, 2.25f, currentWorldPos.z);
-
-                    if (Vector3.Distance(currentWorldPos, nextWorldPos) > 2f)
-                    {
-                        _diagLinePooler[i].transform.position = fixedPosition;
-                        _diagLinePooler[i].transform.rotation =
-                            Quaternion.Euler(_diagLinePooler[i].transform.eulerAngles.x, rotateAngle, _diagLinePooler[i].transform.eulerAngles.z);
-                    }
-                    else
-                    {
-                        _linePooler[i].transform.position = fixedPosition;
-                        _linePooler[i].transform.rotation =
-                            Quaternion.Euler(_linePooler[i].transform.eulerAngles.x, rotateAngle, _linePooler[i].transform.eulerAngles.z);
-                    }
+                    _diagLinePooler[i].transform.position = fixedPosition;
+                    _diagLinePooler[i].transform.rotation =
+                        Quaternion.Euler(_diagLinePooler[i].transform.eulerAngles.x, rotateAngle, _diagLinePooler[i].transform.eulerAngles.z);
+                }
+                else
+                {
+                    _linePooler[i].transform.position = fixedPosition;
+                    _linePooler[i].transform.rotation =
+                        Quaternion.Euler(_linePooler[i].transform.eulerAngles.x, rotateAngle, _linePooler[i].transform.eulerAngles.z);
                 }
             }
+        }
     }
 
 }
