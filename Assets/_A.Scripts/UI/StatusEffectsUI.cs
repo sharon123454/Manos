2b82e6b9c7a3090b49d5ff0110119499e3793418
DSlayer <93089045+sharon123454@users.noreply.github.com>
2023-09-15 12:52:45 +0300

    Update StatusEffectsUI.cs

--- Assets/_A.Scripts/UI/StatusEffectsUI.cs
+++ Assets/_A.Scripts/UI/StatusEffectsUI.cs
@@ -2,7 +2,6 @@ using System.Collections.Generic;
 using System.Collections;
 using UnityEngine;
 using System;
-using static com.zibra.common.Editor.SDFObjects.ServerAuthManager;
 
 [Serializable]
 public struct StatusVisual
