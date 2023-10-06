c9dea5deac2a73b53ec59d00b8c99e8a4d3553d3
DSlayer <93089045+sharon123454@users.noreply.github.com>
2023-10-06 03:04:05 +0300

    Main menu almost finished

--- Assets/_Video/OnVideoEnd.cs
+++ Assets/_Video/OnVideoEnd.cs
@@ -1,18 +1,38 @@
 using UnityEngine.SceneManagement;
 using UnityEngine.InputSystem;
 using UnityEngine.Video;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class OnVideoEnd : MonoBehaviour
 {
-    private VideoPlayer _videoPlayer;
+    [SerializeField] private GameObject mainMenue;
+    [SerializeField] private GameObject creditsWindow;
+    [SerializeField] private Button startButton;
+    [SerializeField] private Button creditButton;
+    [SerializeField] private Button quitButton;
+
     public InputAction skip;
+
+    private VideoPlayer _videoPlayer;
 
     private void Awake()
     {
         _videoPlayer = GetComponent<VideoPlayer>();
         _videoPlayer.loopPointReached += VideoPlayer_loopPointReached;
         skip.performed += Skip_performed;
+        startButton.onClick.AddListener(() =>
+        {
+            OnStartButtonClicked();
+        });
+        creditButton.onClick.AddListener(() =>
+        {
+            OnCreditButtonClicked();
+        });
+        quitButton.onClick.AddListener(() =>
+        {
+            OnQuitButtonClicked();
+        });
     }
     private void OnEnable()
     {
@@ -21,17 +41,38 @@ public class OnVideoEnd : MonoBehaviour
     private void OnDisable()
     {
         skip.Disable();
+        quitButton.onClick.RemoveAllListeners();
+        startButton.onClick.RemoveAllListeners();
+        creditButton.onClick.RemoveAllListeners();
+    }
+
+    private void OnStartButtonClicked()
+    {
+        SceneManager.LoadScene(1);
+    }
+    private void OnCreditButtonClicked()
+    {
+        if (creditsWindow)
+            creditsWindow.SetActive(true);
+    }
+    private void OnQuitButtonClicked()
+    {
+#if UNITY_STANDALONE
+        Application.Quit();
+#endif
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
     }
 
     private void Skip_performed(InputAction.CallbackContext obj)
     {
-        print("lol0");
         _videoPlayer.time = _videoPlayer.length - 2;
     }
 
     private void VideoPlayer_loopPointReached(VideoPlayer source)
     {
-        SceneManager.LoadScene(1);
+        mainMenue.SetActive(true);
     }
 
 }
